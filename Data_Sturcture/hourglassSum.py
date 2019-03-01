67d2770737a238e0f10c7a9b72d6d33f03dbead3
SSG2715 <37144657+SSG2715@users.noreply.github.com>
2019-03-01 09:43:22 +0530

    Update hourglassSum.py

--- Data_Sturcture/hourglassSum.py
+++ Data_Sturcture/hourglassSum.py
@@ -6,4 +6,4 @@ def hourglassSum(arr):
             result = arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]
             list_totals.append(result) # list of all the hourglassSum 
             
-    return max(list_totals)    #largest hourglassSum in arr
+    return max(list_totals)    # largest hourglassSum in arr
